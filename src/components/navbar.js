8590ef4dd2d52d57c62c7f772ff774134244867f
Shifa Haider <shifa.haider065@gmail.com>
2018-11-25 22:50:25 +0500

    continue app

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,10 +6,8 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
 import DialogActions from '@material-ui/core/DialogActions';
 import firebase from 'firebase'
-import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import ViewItem from './view-item.js'
 import Avatar from '@material-ui/core/Avatar';
