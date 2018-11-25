37778cb84e32d6ced8df9ecab93b04e9fc782955
Shifa Haider <shifa.haider065@gmail.com>
2018-11-25 22:46:00 +0500

    continue app

--- src/components/post.js
+++ src/components/post.js
@@ -7,7 +7,6 @@ class Post extends Component {
 
     constructor(props) {
         super(props);
-        console.log(this.props);
         this.item = JSON.parse(localStorage.getItem('item'));
     }
 
