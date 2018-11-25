37c3804d001d8462fd7d64ba54ceb6e2fd21f56a
Shifa Haider <shifa.haider065@gmail.com>
2018-11-25 22:42:45 +0500

    continue app

--- src/components/view-item.js
+++ src/components/view-item.js
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Post from './post.js'
