0dc6d1c994b90fa396b5d90853db806a5336d29f
Shifa Haider <shifa.haider065@gmail.com>
2018-11-25 22:41:37 +0500

    continue app

--- src/components/post.js
+++ src/components/post.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import ToolBar from './navbar.js';
