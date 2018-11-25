de20ec0b4a556325bd30aa97ad18b918cabb6bf5
Shifa Haider <shifa.haider065@gmail.com>
2018-11-25 22:38:59 +0500

    continue app

--- src/components/post.js
+++ src/components/post.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import ToolBar from './toolbar.js';
+import ToolBar from './navbar.js';
 import './style.css'
 
 class Post extends Component {
