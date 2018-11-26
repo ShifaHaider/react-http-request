4ac36840ad2cbb975ed2758ffa67b2f36922b064
Shifa Haider <shifa.haider065@gmail.com>
2018-11-26 20:46:27 +0500

    Adding map

--- src/components/auto-complete.js
+++ src/components/auto-complete.js
@@ -70,4 +70,4 @@ class LocationSearchInput extends Component {
 }
 export default LocationSearchInput;
 //<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUSnrEYsPlAa9LH98TYEX0MtqGZqF2E78&libraries=places&callback=initMap"
-//        a
+//
