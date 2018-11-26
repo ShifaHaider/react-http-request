5f358c67a64555fd01aacf6708910cd39c444660
Shifa Haider <shifa.haider065@gmail.com>
2018-11-26 20:47:51 +0500

    Adding map

--- src/components/auto-complete.js
+++ src/components/auto-complete.js
@@ -69,4 +69,4 @@ class LocationSearchInput extends Component {
     }
 }
 export default LocationSearchInput;
-//<script src="https://maps..com/maps/api/js?key=AIzaSyBUSnrEYsPlAa9LH98TYEX0MtqGZqF2E78&libraries=places&callback=in
+//<script src="https://maps..com/ps/api/js?key=AIzaSyBUSnrEYsPlAa9LH98TYEX0MtqGZqF2E78&libraries=places&callback=in
